accb26989e30ae04df6ad2fb09e1e14d8e46a79d
10Shristi <113353500+10Shristi@users.noreply.github.com>
2024-04-28 15:09:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+const accessKey = 'm_1FQvz8tomMZLcSe3d-1gvOM6mvuh7iOLfA9zJTRSs';
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('.search-input');
+const imagesContainer = document.querySelector('.images-container');
+const loadMoreBtn = document.querySelector('.loadMoreBtn');
+
+let page=1;
+
+const fetchImages = async (query, pageNo) => {
+    try{
+        if(pageNo === 1){
+            imagesContainer.innerHTML ='';
+            }
+            const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            if(data.results.length > 0){
+                data.results.forEach(photo => {
+                    const imageElement = document.createElement('div');
+                    imageElement.classList.add('imageDiv');
+                    imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
+            
+                    const overlayElement = document.createElement('div');
+                    overlayElement.classList.add('overlay');
+            
+                    const overlayText = document.createElement('h3');
+                    overlayText.innerHTML = `${photo.alt_description}`;
+                    overlayElement.appendChild(overlayText);
+            
+                    imageElement.appendChild(overlayElement);
+                    imagesContainer.appendChild(imageElement);
+                });
+            
+                if(data.total_pages === pageNo){
+                    loadMoreBtn.style.display = "none";
+                }else{
+                    loadMoreBtn.style.display = "block";
+                }
+            }
+            else{
+                imagesContainer.innerHTML= `<h2>No image found.</h2>`
+                if(loadMoreBtn.style.dislay === "block"){
+                    loadMoreBtn.style.dislay = "none";
+                }
+            }
+    } 
+    catch(error){
+        imagesContainer.innerHTML= `<h2>Failed to fetch image.Please try againn later.</h2>`;
+    }
+    
+
+    
+}
+
+searchForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const inputText =searchInput.value.trim();
+    if(inputText !==''){
+        page = 1;
+        fetchImages(inputText, page);
+    }
+    else{
+        imagesContainer.innerHTML= `<h2>Please enter a search query.</h2>`
+        if(loadMoreBtn.style.dislay === "block"){
+            loadMoreBtn.style.dislay = "none";
+        }
+    }
+
+ });
+ loadMoreBtn.addEventListener('click',() =>{
+    fetchImages(searchInput.value.trim(), ++page);
+ });
